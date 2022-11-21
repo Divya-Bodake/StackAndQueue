3def70db0fbfec6c52ba2f0bd31ffda277c2ffe7
Divya-Bodake <divyabodake25@gmail.com>
2022-11-21 23:50:56 +0530

    [Divya]Add:Create Stack And Push The Element

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StackAndQueue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(56);
+            linkedListStack.Push(30);
+            linkedListStack.Push(70);
+            linkedListStack.Display();
+
+        }
+    }
+}
